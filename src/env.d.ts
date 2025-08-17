d659bf9ebc63deb320cad3b7f5d89d1ceb93be41
Vlad <lreqr03@gmail.com>
2025-08-17 13:29:15 +0100

    json fix, interfaces for json data

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,20 @@
 /// <reference types="vite/client" />
+export interface BatteryEntry {
+  academyId: number;
+  batteryLevel: number;
+  employeeId: string;
+  serialNumber: string;
+  timestamp: string;
+}
+
+export interface DeviceUsage {
+  serialNumber: string;
+  academyId: number;
+  avg_daily: number | 'unknown';
+}
+
+export interface AcademySummary {
+  academyId: number;
+  badDevices: string[];
+  numBad: number;
+}
